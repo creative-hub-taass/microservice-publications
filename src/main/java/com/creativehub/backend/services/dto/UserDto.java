c8746561e4c21cd20985883f6312a5af1c480d0d
Alessandro Contenti <a.contenti@gmail.com>
2022-04-26 22:27:39 +0200

    fix UserDto
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- src/main/java/com/creativehub/backend/services/dto/UserDto.java
+++ src/main/java/com/creativehub/backend/services/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.creativehub.backend.services.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -10,6 +11,7 @@ import java.util.UUID;
  * Reduced version on UserDto
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto implements Serializable {
 	private final UUID id;
 	private final String username;
